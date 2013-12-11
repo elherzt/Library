dbaecc53572687e5149dda0b2a8b6ab728be248d
Jesus Romero <elherzt@gmail.com>
2013-12-10 18:50:18 -0600

    created test to validate year in add book

--- spec/javascripts/BookSpec.js
+++ spec/javascripts/BookSpec.js
@@ -2,7 +2,7 @@ describe ("BookYear", function(){
   it("should validate year to create a book", function(){
     loadFixtures("book_form.html");
     $("#book-year").val("something");
-    $("#book-year").focusout();
+    $("#book-year").blur();
     expect($("#book_year_error")).toHaveText("Invalid Year");
   });
 });
